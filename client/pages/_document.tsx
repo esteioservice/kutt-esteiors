79f001faaee1e474a8364572a2299fcd1a6acc12
esteioservice <58089317+esteioservice@users.noreply.github.com>
2021-12-01 16:24:31 -0300

    update tradução
    linhas 35, 62, 71

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -32,7 +32,7 @@ class AppDocument extends Document<Props> {
           />
           <meta
             name="description"
-            content={`${publicRuntimeConfig.SITE_NAME} is a free and open source URL shortener with custom domains and stats.`}
+            content={`${publicRuntimeConfig.SITE_NAME} é um encurtador de URL gratuito e estatísticas.`}
           />
           <link
             href="https://fonts.googleapis.com/css?family=Nunito:300,400,700"
@@ -59,7 +59,7 @@ class AppDocument extends Document<Props> {
           />
           <meta
             property="og:description"
-            content="Free & Open Source Modern URL Shortener"
+            content="Encurtador de URL moderno gratuito"
           />
           <meta
             name="twitter:url"
@@ -68,7 +68,7 @@ class AppDocument extends Document<Props> {
           <meta name="twitter:title" content={publicRuntimeConfig.SITE_NAME} />
           <meta
             name="twitter:description"
-            content="Free & Open Source Modern URL Shortener"
+            content="Encurtador de URL moderno gratuito"
           />
           <meta
             name="twitter:image"
